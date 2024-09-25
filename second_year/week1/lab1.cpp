5dfc3b08349d8a10ea845bda6c3297351cd27918
Sley HORTES <sleyhortes13@gmail.com>
2024-09-25 12:57:29 +0800

    2nd Year --> Week 1 Done

--- second_year/week1/lab1.cpp
+++ second_year/week1/lab1.cpp
@@ -1,25 +1,45 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 
 void allocArray(int ***p, int m, int n)
 {
+    *p = (int **)malloc(m * sizeof(int *));  // Allocate memory for row pointers
+    if (*p == NULL)
+    {
+        printf("Memory allocation failed!\n");
+        exit(1);
+    }
 
+    for (int i = 0; i < m; i++)
+    {
+        (*p)[i] = (int *)malloc(n * sizeof(int));
+        if ((*p)[i] == NULL)
+        {
+            printf("Memory allocation failed for row %d!\n", i);
+            exit(1);
+        }
+    }
 }
 
 int main(void)
 {
-  int **array;
+    int **array;
+    int j, k;
 
-  allocArray(&array, 5, 10);
+    allocArray(&array, 5, 10);
 
-  for(int j{0}; j < 5; j++)
-  {
-    for(int k{0}; k < 10; k++)
-      array[j][k] = j * 10 + k;
-  }
+    for (j = 0; j < 5; j++)
+    {
+        for (k = 0; k < 10; k++)
+            array[j][k] = j * 10 + k;
+    }
 
-  for(int j{0}; j < 5; j++)
-  {
-    for(int k{0}; k < 10; k++)
-      std::cout << array[j][k] << " ";
-  }
+    for (j = 0; j < 5; j++)
+    {
+        for (k = 0; k < 10; k++)
+            printf("%p ", (void *)&(array[j][k]));
+    }
+
+    for (j = 0; j < 5; j++)
+        free(array[j]);  
 }
--- second_year/week1/lab2.cpp
+++ second_year/week1/lab2.cpp
@@ -1,29 +1,42 @@
 #include <iostream>
-#include <cstdlib>
 
 template<class T>
 class Memory
 {
 public:
-  static T **allocArray(int m, int n)
-  {
-  }
+    static T **allocArray(int m, int n)
+    {
+        T **p = new T*[m];
+        for (int i{0}; i < m; i++)
+            p[i] = new T[n];
+
+        return p;
+    }
+};
+
+void freeArray(int **p, int rows)
+{
+    for (int i = 0; i < rows; i++)
+        delete[] p[i];
+
+    delete[] p;
 }
 
 int main(void)
 {
-  int **array;
-  array = Memory<int>::allocArray(5, 10);
+    int **array = Memory<int>::allocArray(5, 10);
 
-  for(int j{0}; j < 5; j++)
-  {
-    for(int k{0}; k < 10; k++)
-      array[j][k] = j * 10 + k;
-  }
+    for (int j = 0; j < 5; j++)
+    {
+        for (int k = 0; k < 10; k++)
+            array[j][k] = j * 10 + k;
+    }
 
-  for(int j{0}; j < 5; j++)
-  {
-    for(int k{0}; k < 10; k++)
-      std::cout << array[j][k] <<" ";
-  }
+    for (int j = 0; j < 5; j++)
+    {
+        for (int k = 0; k < 10; k++)
+            std::cout << array[j][k] << " ";
+    }
+
+    freeArray(array, 5);
 }
